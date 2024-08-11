ecae843b70de2e3a77da4a1bcb977204c06155f3
osamanazar47 <osamanazar47@gmail.com>
2024-08-11 20:43:27 +0300

    readme file semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,12 +3,12 @@ const request = require('request');
 const APIUrl = 'https://swapi-api.hbtn.io/api/films';
 
 if (process.argv.length > 2) {
-	request(`${APIUrl}/${process.argv[2]}/`, (err, _, body) => {
-		if (err) {
-			console.log(err);
-		}
-		const characterUrl = JSON.parse(body).characters;
-		const charactersName = characterUrl.map(
+  request(`${APIUrl}/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const characterUrl = JSON.parse(body).characters;
+    const charactersName = characterUrl.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
@@ -16,9 +16,9 @@ if (process.argv.length > 2) {
           }
           resolve(JSON.parse(charactersReqBody).name);
         });
-			}));
-			Promise.all(charactersName)
-				.then(names => console.log(names.join('\n')))
-				.catch(allError => console.log(allError))
-	})
+      }));
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allError => console.log(allError));
+  });
 }
