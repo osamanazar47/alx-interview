12b0dee0e15d48267448b9426b4246252de07958
osamanazar47 <osamanazar47@gmail.com>
2024-08-11 20:33:16 +0300

    starwars api done

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const APIUrl = 'https://swapi-api.hbtn.io/api/films';
+
+if (process.argv.length > 2) {
+	request(`${APIUrl}/${process.argv[2]}/`, (err, _, body) => {
+		if (err) {
+			console.log(err);
+		}
+		const characterUrl = JSON.parse(body).characters;
+		const charactersName = characterUrl.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+			}));
+			Promise.all(charactersName)
+				.then(names => console.log(names.join('\n')))
+				.catch(allError => console.log(allError))
+	})
+}
